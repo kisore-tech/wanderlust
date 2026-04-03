1bc5326cce2417b53cca5e7257b25479ed4c1776
chevvuri kishore <Chevvuri Kishore>
2026-04-03 21:12:51 +0530

    fix error view render

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use((req, res, next) => {
 // error middleware
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).render("listings/error.ejs",{err});
+    res.status(statusCode).render("listings/error",{err});
     // res.status(statusCode).send(message);
 });
 
